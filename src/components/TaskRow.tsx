c616fd3800c2de21df2f75295c11c1e8b4167cb8
Yasuyuki ENDO <eyasuyuki@gmail.com>
2020-07-19 10:59:37 +0900

    add packge settings

--- src/components/TaskRow.tsx
+++ src/components/TaskRow.tsx
@@ -92,7 +92,9 @@ const TaskRow: React.FC<{ data: ITask }> = (props) => {
         <TaskName>{data.taskName}</TaskName>
         <Deadline>⏰{deadlineString}</Deadline>
       </TaskBody>
-      <TaskRemove className="deleteButton" onClick={onDeleteClick}>❌</TaskRemove>
+      <TaskRemove className="deleteButton" onClick={onDeleteClick}>
+        ❌
+      </TaskRemove>
     </Task>
   );
 };
